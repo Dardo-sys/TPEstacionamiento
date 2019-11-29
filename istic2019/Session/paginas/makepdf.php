a1340f682f447141d92e2c75b797db598f153673
Your Name <you@example.com>
2019-11-29 17:57:39 -0300

    pdf print

---
+++ istic2019/Session/paginas/makepdf.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '../vendor/autoload.php';
+
+//variables
+  $patente = $_POST["patente"];
+  $cobrar = $_POST['cobrar'];
+  $ingreso = $_POST['ingreso'];
+  $salida = $_POST['salida'];
+
+//instancia pdf
+  $mpdf = new \Mpdf\Mpdf();
+
+
+//crear pdf
+$data = '';
+
+$data .='<h1>Su Ticket</h1>';
+
+//agregar data
+$data .= '<strong>Vehiculo:</strong>' .$patente.'<br>';
+$data .= '<strong>Hora de ingreso:</strong>' .date("d-m-y H:i",$ingreso).'<br>';
+$data .= '<strong>Hora de salida:</strong>' .date("d-m-y H:i",$salida).'<br>';
+$data .= '<strong>Cobrar:</strong> $' .$cobrar.'<br>';
+
+//escribir pdf
+$mpdf->WriteHTML($data);
+
+//salida a navegador
+$mpdf->Output('TicketParking.pdf', 'D')
